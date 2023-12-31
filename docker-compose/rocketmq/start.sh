405735f3cd04ca509ba1a8e289612cfefcb19b73
axinger <liu_weixing@qq.com>
2023-12-31 21:49:23 +0800

    update

--- docker-compose/rocketmq/start.sh
+++ docker-compose/rocketmq/start.sh
@@ -1,19 +1,16 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 # 创建目录
-mkdir -p ./rmqs/logs
-mkdir -p ./rmqs/store
-mkdir -p ./rmq/logs
-mkdir -p ./rmq/store
+# mkdir -p ./nameserver/{logs,store}
+# mkdir -p ./broker/{logs,store,conf}
+
 
 # 设置目录权限
-chmod -R 777 ./rmqs/logs
-chmod -R 777 ./rmqs/store
-chmod -R 777 ./rmq/logs
-chmod -R 777 ./rmq/store
+chmod -R 777 ./nameserver/{logs,store}
+chmod -R 777 ./broker/{logs,store,conf}
+
 
 # 下载并启动容器，且为 后台 自动启动
-docker-compose up -d
+docker compose up -d
 
 
 # 显示 rocketmq 容器
